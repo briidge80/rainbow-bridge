6a650d84a2d131d98e7367ed333091a46000fbe0
Briidge <briidge80@gmail.com>
2024-04-06 15:51:11 -0600

    Remove unnecessary NgFor import

--- src/app/region/region.component.ts
+++ src/app/region/region.component.ts
@@ -1,13 +1,12 @@
 import {Component, Input} from '@angular/core';
 import {Region} from '../../model/region';
-import {NgFor} from '@angular/common';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
     selector: 'bridge-region',
     standalone: true,
-    imports: [NgFor, MatCheckboxModule, MatDividerModule],
+    imports: [MatCheckboxModule, MatDividerModule],
     templateUrl: './region.component.html',
     styleUrl: './region.component.less',
 })
